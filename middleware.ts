252d7dc5008b8fb1a667c38267e1cf60ef418d71
yangxdev <yangxdev@gmail.com>
2024-03-21 23:10:15 +0100

    Middleware update

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,11 @@ import GlobalConfig from "@/app/app.config";
 export async function middleware(request: NextRequest) {
     const storedRates = cookies().get("conversionRates");
     const storedDate = cookies().get("conversionRatesDate");
-    if (storedRates && storedDate) {
-        // console.log("middleware: stored ConversionRates found:");
-        // console.log(storedRates);
-        // console.log(storedDate);
-        // console.log("--------------------");
-    } else {
+    if (
+        !storedRates ||
+        !storedDate ||
+        new Date(storedDate.value) < new Date()
+    ) {
         const response = NextResponse.next();
         const APIresponse = await fetch(
             `https://v6.exchangerate-api.com/v6/9c90d2094ff9dfae6d61f3c8/latest/${GlobalConfig.baseCurrency}`
@@ -18,10 +17,7 @@ export async function middleware(request: NextRequest) {
         const data = await APIresponse.json();
         const rates = data.conversion_rates;
 
-        response.cookies.set(
-            "conversionRates",
-            JSON.stringify(rates)
-        );
+        response.cookies.set("conversionRates", JSON.stringify(rates));
         response.cookies.set(
             "conversionRatesDate",
             new Date().toISOString().split("T")[0]
