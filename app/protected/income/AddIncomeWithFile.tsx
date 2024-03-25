70b4ecef3d1c87f5b7cd5945a6d2cd67f931454e
yangxdev <yangxdev@gmail.com>
2024-03-25 23:05:31 +0100

    CSV import data check, modal update

--- app/protected/income/AddIncomeWithFile.tsx
+++ app/protected/income/AddIncomeWithFile.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef, useState } from "react";
 import AddIncomeWithFileModal from "./AddIncomeWithFileModal";
+import DOMPurify from "dompurify";
 
 export default function AddIncomeWithFile() {
     const fileInputRef = useRef<HTMLInputElement>(null);
@@ -8,13 +9,156 @@ export default function AddIncomeWithFile() {
     const [incomeData, setIncomeData] = useState<any[]>([]);
     const [openDialog, setOpenDialog] = useState<boolean>(false);
 
+    function checkInput(data: { date: string; source: string; amount: string; currency: string; category: string; notes: string }) {
+        function dateCheck(date: string) {
+            if (date === undefined || date === null || date === "") {
+                return false;
+            }
+
+            const purifiedDate = DOMPurify.sanitize(date);
+            if (purifiedDate !== date) {
+                return false;
+            }
+
+            if (date.length > 10) {
+                return false;
+            }
+
+            const dateArray = date.split("-");
+            if (dateArray.length !== 3) {
+                return false;
+            }
+            const month = parseInt(dateArray[0]);
+            const day = parseInt(dateArray[1]);
+            const year = parseInt(dateArray[2]);
+
+            if (month < 1 || month > 12) {
+                return false;
+            }
+            if (day < 1 || day > 31) {
+                return false;
+            }
+            if (year < 2000 || year > 2100) {
+                return false;
+            }
+            const monthsWith30Days = [4, 6, 9, 11];
+            if ((month === 2 && day > 28) || (monthsWith30Days.includes(month) && day > 30)) {
+                return false;
+            }
+
+            return true;
+        }
+        function sourceCheck(source: string) {
+            if (source === undefined || source === null || source === "") {
+                return false;
+            }
+
+            const purifiedSource = DOMPurify.sanitize(source);
+            if (purifiedSource !== source) {
+                return false;
+            }
+
+            if (source.length > 100) {
+                return false;
+            }
+
+            return true;
+        }
+        function amountCheck(amount: string) {
+            if (amount === undefined || amount === null || amount === "") {
+                return false;
+            }
+
+            const purifiedAmount = DOMPurify.sanitize(amount);
+            if (purifiedAmount !== amount) {
+                return false;
+            }
+
+            if (amount.length > 10) {
+                return false;
+            }
+
+            const amountNumber = parseFloat(amount);
+            if (isNaN(amountNumber)) {
+                return false;
+            }
+
+            if (amountNumber < 0) {
+                return false;
+            }
+
+            return true;
+        }
+        function currencyCheck(currency: string) {
+            if (currency === undefined || currency === null || currency === "") {
+                return false;
+            }
+
+            const purifiedCurrency = DOMPurify.sanitize(currency);
+            if (purifiedCurrency !== currency) {
+                return false;
+            }
+
+            if (currency.length !== 1 && currency.length !== 3) {
+                return false;
+            }
+
+            return true;
+        }
+        function categoryCheck(category: string) {
+            if (category === undefined || category === null || category === "") {
+                return false;
+            }
+
+            const purifiedCategory = DOMPurify.sanitize(category);
+            if (purifiedCategory !== category) {
+                return false;
+            }
+
+            if (category.length > 100) {
+                return false;
+            }
+
+            return true;
+        }
+        function notesCheck(notes: string) {
+            if (notes === undefined || notes === null || notes === "") {
+                return true;
+            }
+
+            const purifiedNotes = DOMPurify.sanitize(notes);
+            if (purifiedNotes !== notes) {
+                return false;
+            }
+
+            if (notes.length > 1000) {
+                return false;
+            }
+
+            return true;
+        }
+
+        const date = data.date;
+        const source = data.source;
+        const amount = data.amount;
+        const currency = data.currency;
+        const category = data.category;
+        const notes = data.notes;
+
+        if (!dateCheck(date) || !sourceCheck(source) || !amountCheck(amount) || !currencyCheck(currency) || !categoryCheck(category) || !notesCheck(notes)) {
+            return false;
+        }
+
+        return true;
+    }
+
     const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = async (e) => {
-                const text = (e.target?.result as string);
-                const lines = text.split('\n');
+                const text = e.target?.result as string;
+                const lines = text.split("\n");
                 let incomeData = [];
                 for (let line of lines) {
                     // example:
@@ -28,20 +172,28 @@ export default function AddIncomeWithFile() {
                     */
                     // Replace commas within quotes
                     line = line.replace(/"([^"]*)"/g, function (match) {
-                        return match.replace(/,/g, '');
+                        return match.replace(/,/g, "");
                     });
+                    if (!line) continue;
 
-                    const data = line.split(',');
+                    const data = line.split(",");
                     const date = data[1];
                     const source = data[2];
-                    const amount = data[3].replace(/[$"]/g, '');
-                    const currencySymbol = data[3].replace(/[^$]/g, '');
-                    const currency = currencySymbol === '$' ? 'USD' : 'EUR';
+                    const amount = data[3].replace(/[$"]/g, "");
+                    const currencySymbol = data[3].replace(/[^$]/g, "");
+                    const currency = currencySymbol === "$" ? "USD" : "EUR";
                     const category = data[4];
                     const notes = data[5];
-                    incomeData.push({ date, source, amount, currency, category, notes });
+                    if (checkInput({ date, source, amount, currency, category, notes })) {
+                        const enabled = true;
+                        incomeData.push({ enabled, date, source, amount, currency, category, notes });
+                    } else {
+                        console.log("Invalid input: " + line);
+                        const enabled = false;
+                        incomeData.push({ enabled, date, source, amount, currency, category, notes });
+                    }
                 }
-                console.log(incomeData);
+                // console.log(incomeData);
                 setIncomeData(incomeData);
                 setOpenDialog(true);
             };
@@ -65,7 +217,7 @@ export default function AddIncomeWithFile() {
             <AddIncomeWithFileModal
                 isOpen={openDialog}
                 incomeData={incomeData}
-                handleClose={() => { 
+                handleClose={() => {
                     setOpenDialog(false);
                     if (fileInputRef.current) {
                         fileInputRef.current.value = "";
@@ -73,16 +225,11 @@ export default function AddIncomeWithFile() {
                 }}
             />
             <div className="font-bold pb-3">Import CSV</div>
-            <input
-                type="file"
-                accept=".csv"
-                className="hidden"
-                ref={fileInputRef}
-                onChange={handleFileUpload}
-            />
+            <input type="file" accept=".csv" className="hidden" ref={fileInputRef} onChange={handleFileUpload} />
             <button className="w-full" onClick={() => fileInputRef.current?.click()}>
                 <div className="transition duration-100 bg-[#434343] rounded-md hover:bg-[#565656] p-5">
-                    <strong>Choose a file</strong><br></br> or drag it here
+                    <strong>Choose a file</strong>
+                    <br></br> or drag it here
                 </div>
             </button>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,8 @@ const config: Config = {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('tailwind-scrollbar'),
+    ],
 };
 export default config;
