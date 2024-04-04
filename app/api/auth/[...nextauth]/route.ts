009a20a04af5e24c4975e9bf8c02ff0ba2c3bea8
yangxdev <yangxdev@gmail.com>
2024-04-04 22:22:02 +0200

    Vercel hotfix #2: authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,21 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+// import GitHubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/app/utils/authOptions";
 
-export const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID ?? "",
-            clientSecret: process.env.GOOGLE_SECRET ?? "",
-        })
-    ],
-};
+// export const authOptions = {
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID ?? "",
+//             clientSecret: process.env.GITHUB_SECRET ?? "",
+//         }),
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_ID ?? "",
+//             clientSecret: process.env.GOOGLE_SECRET ?? "",
+//         })
+//     ],
+// };
 
-export const handler = NextAuth({
-    ...authOptions,
-    callbacks: {
-        async signIn(params) {
-            const { user, account, profile } = params;
-            return true;
-        }
-    }
-});
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
