e9744bbf2ff7718a7e1d7bbdca7145d7d4306c1b
yangxdev <yangxdev@gmail.com>
2024-04-04 22:07:40 +0200

    cuid update

--- app/api/database/add_income/route.ts
+++ app/api/database/add_income/route.ts
@@ -1,10 +1,12 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
-import cuid from "cuid";
+// import cuid from "cuid";
+import { createId } from "@paralleldrive/cuid2";
 
 export async function POST(request: Request) {
     const { source, date, amount, currency, category, notes } = await request.json();
-    const id = cuid();
+    // const id = cuid();
+    const id = createId();
     const userId = "cltyanrp50000rbp159e9j2i8";
     const createdAt = new Date().toISOString();
     const updatedAt = new Date().toISOString();
