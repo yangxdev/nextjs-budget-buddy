971e937d5c5627d614661ab435ae498651c5704c
yangxdev <yangxdev@gmail.com>
2024-04-14 22:04:12 +0200

    Skeleton pre-impl.

---
+++ app/protected/payment/loading.js
@@ -0,0 +1,9 @@
+import { Skeleton } from "../../components/ui/skeleton";
+
+export default function SkeletonCard() {
+    return (
+        <div>
+            <Skeleton height="40px" width="100%" />
+        </div>
+    )
+}
