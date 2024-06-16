54deef3fc43ef31071d5c4efe6df9021fe42f625
yangxdev <yangxdev@gmail.com>
2024-06-16 19:01:13 +0200

    Expenses: Ant-design impl. [start]

--- app/protected/expenses/AddExpense.tsx
+++ app/protected/expenses/AddExpense.tsx
@@ -6,6 +6,8 @@ import addRandomExpenses from "@/app/api/database/add_random_expenses/addRandomE
 import { useRouter } from "next/navigation";
 import VanillaTilt from "vanilla-tilt";
 import { CgDice5 } from "react-icons/cg";
+import { Button } from 'antd';
+import { StyleProvider } from '@ant-design/cssinjs';
 
 const defaultLanguage = GlobalConfig.i18n.defaultLanguage || "en";
 const gc = GlobalConfig.i18n.translations[defaultLanguage as keyof typeof GlobalConfig.i18n.translations]?.expenses?.addExpense?.addExpenseForm;
@@ -79,70 +81,91 @@ export default function AddExpense() {
     }, []);
 
     return (
-        <div ref={elementRef} className="p-5 bg-white border-[1px] border-lightBorder max-w-80 rounded-2xl text-sm select-none h-min">
-            <div className="font-bold pb-2 text-lg">{gc?.title}</div>
-            <div className="pb-2">
-                {gc?.date}
-                <input type="date" ref={dateRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md" defaultValue={currentDate} required />
-            </div>
-            <div className="pb-2">
-                {gc?.source}
-                <input type="text" ref={sourceRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 hover:bg-white transition duration-100 focus:outline-none dark:[color-scheme:dark] shadow-sm hover:shadow-md" required />
-            </div>
-            <div className="pb-2 flex flex-row gap-4">
-                <div className="flex flex-col w-1/2">
-                    {gc?.amount}
-                    <input
-                        type="number"
-                        ref={amountRef}
-                        className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2
+        <StyleProvider hashPriority="high">
+            <div ref={elementRef} className="p-5 bg-white border-[1px] border-lightBorder max-w-80 rounded-2xl text-sm select-none h-min">
+                <div className="font-bold pb-2 text-lg">{gc?.title}</div>
+                <div className="pb-2">
+                    {gc?.date}
+                    <input type="date" ref={dateRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md" defaultValue={currentDate} required />
+                </div>
+                <div className="pb-2">
+                    {gc?.source}
+                    <input type="text" ref={sourceRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 hover:bg-white transition duration-100 focus:outline-none dark:[color-scheme:dark] shadow-sm hover:shadow-md" required />
+                </div>
+                <div className="pb-2 flex flex-row gap-4">
+                    <div className="flex flex-col w-1/2">
+                        {gc?.amount}
+                        <input
+                            type="number"
+                            ref={amountRef}
+                            className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2
                         [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md
                         "
-                        required
-                    />
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col w-1/2">
+                        {gc?.currency}
+                        <select required ref={currencyRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md">
+                            {GlobalConfig.currency.currencies.map((currency, index) => (
+                                <option key={index} value={currency}>
+                                    {currency}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
                 </div>
-                <div className="flex flex-col w-1/2">
-                    {gc?.currency}
-                    <select required ref={currencyRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md">
-                        {GlobalConfig.currency.currencies.map((currency, index) => (
-                            <option key={index} value={currency}>
-                                {currency}
+                <div className="pb-2">
+                    {gc?.category}
+                    <select ref={categoryRef} required className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md">
+                        {GlobalConfig.expenses.expenseCategories.map((category, index) => (
+                            <option key={index} value={category}>
+                                {category}
                             </option>
                         ))}
                     </select>
                 </div>
-            </div>
-            <div className="pb-2">
-                {gc?.category}
-                <select ref={categoryRef} required className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md">
-                    {GlobalConfig.expenses.expenseCategories.map((category, index) => (
-                        <option key={index} value={category}>
-                            {category}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="pb-2">
-                {gc?.notes}
-                <textarea ref={notesRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md" rows={1}></textarea>
-            </div>
-            <div className="flex flex-row justify-end gap-2">
-                {GlobalConfig.debug.showAddRandomEntriesButton && (
-                    <button
-                        onClick={() => {
+                <div className="pb-2">
+                    {gc?.notes}
+                    <textarea ref={notesRef} className="w-full bg-whiteDarker border-[1px] border-lightBorder rounded-md p-2 cursor-pointer hover:bg-white transition duration-100 dark:[color-scheme:dark] focus:outline-none shadow-sm hover:shadow-md" rows={1}></textarea>
+                </div>
+                <div className="flex flex-row justify-end gap-2">
+                    {GlobalConfig.debug.showAddRandomEntriesButton && (
+                        // <button
+                        //     onClick={() => {
+                        //         addRandomExpenses();
+                        //         toast.success("Random Expenses Added", {});
+                        //     }}
+                        //     className="flex flex-row items-center gap-1 transition duration-100 bg-white rounded-md p-2 hover:bg-newGreen-500 hover:text-white"
+                        // >
+                        //     <div>
+                        //         <CgDice5 />
+                        //     </div>
+                        //     <div>{gc?.addRandom}</div>
+                        // </button>
+                        <Button onClick={() => {
                             addRandomExpenses();
                             toast.success("Random Expenses Added", {});
-                        }}
-                        className="flex flex-row items-center gap-1 transition duration-100 bg-white rounded-md p-2 hover:bg-newGreen-500 hover:text-white"
-                    >
-                        <div>
+                        }} type="dashed">
                             <CgDice5 />
-                        </div>
-                        <div>{gc?.addRandom}</div>
-                    </button>
-                )}
-                <button
-                    onClick={() => {
+                            {gc?.addRandom}
+                        </Button>
+                    )}
+                    {/* <button
+                        onClick={() => {
+                            sourceRef.current!.value = "";
+                            dateRef.current!.value = currentDate;
+                            amountRef.current!.value = "";
+                            currencyRef.current!.value = GlobalConfig.currency.baseCurrency;
+                            categoryRef.current!.value = GlobalConfig.expenses.expenseCategories[0];
+                            notesRef.current!.value = "";
+                            toast.success("Fields reset", {});
+                        }}
+                        className="transition duration-100 bg-white rounded-md p-2 hover:bg-newRed-500 hover:text-white"
+                    >
+                        {gc?.reset}
+                    </button> */}
+                    <Button onClick={() => {
                         sourceRef.current!.value = "";
                         dateRef.current!.value = currentDate;
                         amountRef.current!.value = "";
@@ -150,15 +173,16 @@ export default function AddExpense() {
                         categoryRef.current!.value = GlobalConfig.expenses.expenseCategories[0];
                         notesRef.current!.value = "";
                         toast.success("Fields reset", {});
-                    }}
-                    className="transition duration-100 bg-white rounded-md p-2 hover:bg-newRed-500 hover:text-white"
-                >
-                    {gc?.reset}
-                </button>
-                <button onClick={handleSubmit} className="transition border-[1px] shadow-sm hover:shadow-md duration-100 bg-white rounded-md p-2 hover:bg-newGreen-500 hover:text-white">
+                    }} type="default" danger>{gc?.reset}</Button>
+
+                    {/* <button onClick={handleSubmit} className="transition border-[1px] shadow-sm hover:shadow-md duration-100 bg-white rounded-md p-2 hover:bg-newGreen-500 hover:text-white">
                     {gc?.add}
-                </button>
+                </button> */}
+                    <Button onClick={handleSubmit} type="primary">{gc?.add}</Button>
+                </div>
             </div>
-        </div>
+        </StyleProvider>
     );
 }
+
+// Thanks @phatify (https://github.com/ant-design/ant-design/issues/38794) for the StyleProvider fix, that solves the issue of Tailwind CSS overriding Ant Design's classes.
